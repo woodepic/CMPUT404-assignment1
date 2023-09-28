f8d1d773447098f6b9ec07b25e20bcc87ef1276f
Matthew Wood <mwood2@cs.ualberta.ca>
2023-09-28 09:19:38 -0600

    testing git setup on lab machine

--- freetests.py
+++ freetests.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright 2013 Abram Hindle
+# Copyright 2013 Abram Hindle 
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
